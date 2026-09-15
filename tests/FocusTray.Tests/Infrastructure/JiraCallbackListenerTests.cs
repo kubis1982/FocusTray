43f58b0e4478b5992e8f33102bf7edfbdf2af35b
m.swiatnicki <m.swiatnicki@7technology.pl>
2026-09-15 13:51:57 +0200

    feat(jira): add OAuth2 loopback callback listener

---
+++ tests/FocusTray.Tests/Infrastructure/JiraCallbackListenerTests.cs
@@ -0,0 +1,40 @@
+using AwesomeAssertions;
+using FocusTray.Infrastructure.Jira;
+using Xunit;
+
+namespace FocusTray.Tests.Infrastructure;
+
+public class JiraCallbackListenerTests
+{
+    [Fact]
+    public void Should_ParseCodeAndState_When_QueryContainsSuccessParameters()
+    {
+        var result = JiraCallbackListener.ParseCallbackQuery("?code=abc123&state=xyz789");
+
+        result.Code.Should().Be("abc123");
+        result.State.Should().Be("xyz789");
+        result.Error.Should().BeNull();
+    }
+
+    [Fact]
+    public void Should_ParseErrorFields_When_QueryContainsAccessDenied()
+    {
+        var result = JiraCallbackListener.ParseCallbackQuery(
+            "?error=access_denied&error_description=User%20denied%20access&state=xyz789");
+
+        result.Error.Should().Be("access_denied");
+        result.ErrorDescription.Should().Be("User denied access");
+        result.State.Should().Be("xyz789");
+        result.Code.Should().BeNull();
+    }
+
+    [Fact]
+    public void Should_ReturnEmptyResult_When_QueryIsEmpty()
+    {
+        var result = JiraCallbackListener.ParseCallbackQuery("");
+
+        result.Code.Should().BeNull();
+        result.State.Should().BeNull();
+        result.Error.Should().BeNull();
+    }
+}
